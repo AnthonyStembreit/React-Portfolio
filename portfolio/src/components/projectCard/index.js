88aa08be4192dc9462a5e710924482e1abc7a351
AnthonyStembreit <stembreit@gmail.com>
2020-10-29 15:34:39 -0500

    added some css styling to the project cards, created projectCard directory

--- portfolio/src/components/projectCard/index.js
+++ portfolio/src/components/projectCard/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import './projectCard.css'
 
 export default function ProjectCard(props) {
     console.log(props)
 
     return (
-        <Card >
+        <Card class=" card projectCard" id={props.id}>
             <Card.Header>{props.name}</Card.Header>
             <Card.Body>
-                <Card.Text>
-                    <img src={props.image} alt={props.name}></img>
-                 </Card.Text>
+                <Card.Img src={props.image} alt={props.name}></Card.Img>
             </Card.Body>
             <Card.Footer >
                 <Button variant="dark"><a href={props.deployed} target="_blank">Deployed</a></Button>
