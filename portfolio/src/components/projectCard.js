ec2cf4434b17c0a8e959a9bae2dba7e5c283aa95
AnthonyStembreit <stembreit@gmail.com>
2020-10-29 14:45:50 -0500

    added a project.json to hold project data, creatd a card compnonent that is used multiple times in the project page

--- portfolio/src/components/projectCard.js
+++ portfolio/src/components/projectCard.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
-export default function ProjectCard(){
-    return(
-        <Card>
-            project card
+export default function ProjectCard(props) {
+    console.log(props)
+
+    return (
+        <Card >
+            <Card.Header>{props.name}</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    <img src={props.image} alt={props.name}></img>
+                 </Card.Text>
+            </Card.Body>
+            <Card.Footer >
+                <Button variant="dark"><a href={props.deployed} target="_blank">Deployed</a></Button>
+                <Button variant="dark"><a href={props.github} target="_blank">Github</a></Button>
+                <Button variant="dark">icon</Button>
+            </Card.Footer>
         </Card>
     )
 }
--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProjectCard from '../components/projectCard'
+import projects from '../data/projects.json'
 
 export default function Projects(){
+   const [projectState, setProjectState] = useState([])
+   useEffect(()=> setProjectState(projects), [])
     return(
-        <ProjectCard/>
+        projectState.map(projects => (
+            <ProjectCard
+            id={projects.id}
+            name={projects.name}
+            image={projects.image}
+            tech={projects.tech}
+            description={projects.description}
+            deployed={projects.deployed}
+            github={projects.github}/>
+        ))
+
     )
 }
