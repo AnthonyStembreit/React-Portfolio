e29f60a749e1fb7958eac54ef7f9778d2430e1bd
AnthonyStembreit <stembreit@gmail.com>
2020-10-31 19:25:15 -0500

    attempted to have an overlay view for description & tech used, running out of time so just put them in the card body

--- portfolio/src/components/overlay/index.js
+++ portfolio/src/components/overlay/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import Tech from './tech'
-import Button from 'react-bootstrap/Button'
 import './overlay.css'
-
-
 
 export default function Overlay(props) {
     return (
-    <div class="overlay" id={props.name + "description"}>
-        <Button id={props.name + "close"}><i class="fas fa-arrow-circle-down"></i></Button>
-        <p>{props.description}</p>
-        <ul>
+    <div class="overlay" id={props.name}>
+        <br></br>
+        <ul class="overlay">
         <Tech tech={props.tech}/>
         </ul>
+        <br></br>
+        <p class="overlay">{props.description}</p>
     </div>
     )
 }
--- portfolio/src/components/projectCard/index.js
+++ portfolio/src/components/projectCard/index.js
@@ -6,25 +6,25 @@ import Col from 'react-bootstrap/Col'
 import './projectCard.css'
 
 
+
 export default function ProjectCard(props) {
     console.log(props)
     return (
         <Col>
-        <Card id={props.name}>
-            <Card.Header>{props.name}</Card.Header>
-            <Card.Body>
-                <Card.Img src={props.image} alt={props.name}></Card.Img>
-            </Card.Body>
-            <Card.Footer >
-                <Button variant="dark"><a href={props.deployed} target="_blank">Deployed</a></Button>
-                <Button variant="dark"><a href={props.github} target="_blank">Github</a></Button>
-                <Button variant="dark" id={props.name + "open"}><i class=" buttonIcon fas fa-arrow-circle-up"></i></Button>
-            </Card.Footer>
-        </Card>
-        <Overlay 
-        id={props.id}
-        tech={props.tech}
-        description={props.description}/>
+            <Card id={props.id}>
+                <Card.Header>{props.name}</Card.Header>
+                <Card.Body>
+                    <Card.Img src={props.image} alt={props.name}></Card.Img>
+                    <Overlay
+                        id={props.id}
+                        tech={props.tech}
+                        description={props.description} />
+                </Card.Body>
+                <Card.Footer >
+                    <Button variant="dark"><a href={props.deployed} target="_blank">Deployed</a></Button>
+                    <Button variant="dark"><a href={props.github} target="_blank">Github</a></Button>
+                </Card.Footer>
+            </Card>
         </Col>
     )
 }
